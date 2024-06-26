ff0016d575d291c507abe891d2bd16dd4f4fdb2b
Rares Dica <dica.rares8@gmail.com>
2024-06-26 22:37:57 +0200

    Renaming tests so they align with the format

---
+++ tests/test___getattr__.py
@@ -0,0 +1,23 @@
+import pytest
+
+from click.core import __getattr__
+from click.core import _BaseCommand
+from click.core import _MultiCommand
+
+
+def test_getattr_base_command():
+    with pytest.warns(
+        DeprecationWarning,
+        match="'BaseCommand' is deprecated and will be removed in Click 9.0. Use "
+        "'Command' instead.",
+    ):
+        assert __getattr__("BaseCommand") == _BaseCommand
+
+
+def test_getattr_multi_command():
+    with pytest.warns(
+        DeprecationWarning,
+        match="'MultiCommand' is deprecated and will be removed in Click 9.0. Use "
+        "'Group' instead.",
+    ):
+        assert __getattr__("MultiCommand") == _MultiCommand
---
+++ tests/test_is_ascii_encoding.py
@@ -0,0 +1,5 @@
+
+from click._compat import is_ascii_encoding 
+
+def test_is_ascii_encoding_invalid_encoding():
+    assert is_ascii_encoding('invalid-encoding') == False
